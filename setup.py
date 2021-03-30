ea675009de315b04e8cde9ba30455697d621d4c9
AkshatDev <akshat.thakur8@gmail.com>
2021-03-30 14:10:55 +0000

    done till flask connection

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="It's Google Colab Env", 
+    author="Luxxi_Ikaros", 
+    packages=find_packages(),
+    license="MIT"
+)
